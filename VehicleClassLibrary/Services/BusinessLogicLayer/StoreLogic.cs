a3d450a41213933ea35596cc9029d667811e6e8e
Patrick Brewster <Patrick.shepherd33@gmail.com>
2026-06-21 14:42:05 -0400

    Add the StoreLogic class to pass data to and from the DAO and presentation layers.

---
+++ VehicleClassLibrary/Services/BusinessLogicLayer/StoreLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleClassLibrary.Services.BusinessLogicLayer
+{
+    internal class StoreLogic
+    {
+    }
+}
